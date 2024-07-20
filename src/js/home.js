5678eb2a7d86a2d7b3b2ac1b13983e808c9a5500
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-07-20 20:55:50 +0300

    save

--- src/js/home.js
+++ src/js/home.js
@@ -3,10 +3,6 @@ const registerModal = document.querySelector(".register__modal");
 const loginButton = document.querySelector(".logButton")
 const loginModal = document.querySelector(".login__modal")
 const cross = document.querySelectorAll(".cross__svg")
-const cross2 = document.querySelector(".cross__svg2")
-console.log(cross[0])
-console.log(registerButton);
-console.log("test");
 
 registerButton.addEventListener("click", (e) => {
     registerModal.classList.remove("change__invisible");
@@ -30,11 +26,4 @@ cross[1].addEventListener("click" , () => {
     registerModal.classList.add("change__invisible");
     console.log(123)
 })
-
-
-// cross2.addEventListener("click" , () => {
-//     loginModal.classList.add("change__invisible");
-//     registerModal.classList.add("change__invisible");
-//     console.log(123)
-// })
 
