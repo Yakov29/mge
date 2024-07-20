a233ffd089500f1e0150df386d8409dc803c01ca
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2024-07-20 11:36:20 +0300

    Update home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,17 +1,17 @@
-const registerButton = document.querySelector(".regButton");
-const registerModal = document.querySelector(".register__modal");
-const loginButton = document.querySelector(".logButton")
-const loginModal = document.querySelector(".login__modal")
-console.log(registerButton);
-console.log("test");
+// const registerButton = document.querySelector(".regButton");
+// const registerModal = document.querySelector(".register__modal");
+// const loginButton = document.querySelector(".logButton")
+// const loginModal = document.querySelector(".login__modal")
+// console.log(registerButton);
+// console.log("test");
 
-registerButton.addEventListener("click", (e) => {
-    registerModal.classList.remove("change__invisible");
-});
+// registerButton.addEventListener("click", (e) => {
+//     registerModal.classList.remove("change__invisible");
+// });
 
 
-loginButton.addEventListener("click", (e) => {
-    loginModal.classList.remove("change__invisible");
-});
+// loginButton.addEventListener("click", (e) => {
+//     loginModal.classList.remove("change__invisible");
+// });
 
 
