fa02d8090b42df63aa8e40c61e587a9ce28b8881
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2024-07-20 19:07:37 +0300

    cursy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./js/home.js";
 import "./js/sponsors.js";
-import "./js/profile/profileSystem.js";
 import "./js/profile/addProfile.js";
+import "./js/profile/profileSystem.js";
 import "./js/reviews/reviewsSystem.js";
+
